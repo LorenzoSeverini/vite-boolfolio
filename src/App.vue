2beda0e8b51ad44f79098050596fe1cf25f4855e
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-17 15:39:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import HeaderTitle from './components/HeaderTitle.vue'
+import ProjectCard from './components/ProjectCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderTitle,
+    ProjectCard,
+  },
+  
+  data() {
+    return {
+      //
+    }
+  },
+
+  methods: {
+    // 
+  },
+
+  mounted() {
+    // 
+  },
+}
+
+</script>
+
+
+<template>
+  <div class="container">
+    <header>
+      <HeaderTitle msg="Hello 🫡" />
+    </header>
+    
+    <main>
+      <ProjectCard />
+    </main>
+
+    <footer>
+      <h2>Vue-Api-Rock</h2>
+    </footer>
+
+  </div>
+</template>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ProjectCard',
+
+  data() {
+    return {
+      apiUrl:'http://localhost:8000/api/',
+      loading: false,
+      projects: [],
+    }
+  },
+
+  methods: {
+    getProjects() {
+      axios.get(this.apiUrl + 'projects').then(response => {
+        console.log(response.data);
+        this.loading = true;
+        this.projects = response.data;
+      })
+
+    }, 
+  },
+
+  mounted() {
+    this.getProjects();
+  } 
+}
+</script>
+
+<template>
+  <!-- project card -->
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
