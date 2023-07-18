646df2fe33e20bf5c80e3ff96b90e6de1d32ff6e
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-18 16:10:39 +0200

    feat home views

---
+++ src/views/AppHome.vue
@@ -0,0 +1,28 @@
+<script>
+import HeaderTitle from '../components/header/HeaderTitle.vue';
+import FooterNav from '../components/footer/FooterNav.vue';
+
+export default {
+  name: 'AppHome',
+    components: {
+        HeaderTitle,
+        FooterNav,
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <header>
+            <HeaderTitle msg="Hello 🫡" />
+        </header>
+    
+        <main>
+            <h1>Home</h1>
+        </main>
+        
+        <footer>
+            <FooterNav />
+        </footer>
+    </div>
+</template>
