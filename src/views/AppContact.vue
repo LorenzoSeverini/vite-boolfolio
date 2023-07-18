9209038ab39f5b5a1b750c8475402aecb8d51460
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-18 16:10:23 +0200

    feat contact view

---
+++ src/views/AppContact.vue
@@ -0,0 +1,28 @@
+<script>
+import HeaderTitle from '../components/header/HeaderTitle.vue';
+import FooterNav from '../components/footer/FooterNav.vue';
+
+export default {
+    name: "AppContact",
+    components: { 
+        HeaderTitle,
+        FooterNav, 
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <header>
+            <HeaderTitle msg="Hello 🫡" />
+        </header>
+        
+        <main>
+            <h1>Contact</h1>
+        </main>
+    
+        <footer>
+            <FooterNav />
+        </footer>
+    </div>
+</template>
