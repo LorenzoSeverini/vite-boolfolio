b7763387d90d4c3800d6c4127239e582777719e1
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-18 16:10:52 +0200

    feat project list views

---
+++ src/views/AppProjectList.vue
@@ -0,0 +1,30 @@
+<script>
+import HeaderTitle from '../components/header/HeaderTitle.vue';
+import ProjectCard from '../components/project/ProjectCard.vue';
+import FooterNav from '../components/footer/FooterNav.vue';
+
+export default {
+  name: 'ProjectList',
+  components: {
+    HeaderTitle,
+    ProjectCard,
+    FooterNav,
+  },
+}
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <HeaderTitle msg="Hello 🫡" />
+    </header>
+  
+    <main>
+      <ProjectCard />
+    </main>
+    
+    <footer>
+        <FooterNav />
+    </footer>
+  </div>
+</template>
