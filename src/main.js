e44adbfc7478cfbd3a69394e3cdf6423c6f48ed1
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-18 16:08:12 +0200

    feat import router and font awesoem

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './styles/style.scss'
 import App from './App.vue'
+import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+library.add(faTwitter, faFacebook, faInstagram);
 
-createApp(App).mount('#app')
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon )
+.use(router)
+.mount('#app')
