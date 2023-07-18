9a6dd7c946d69b4f6d0bb069d8ec4613528c14b2
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-18 16:09:26 +0200

    feat components header title

---
+++ src/components/header/HeaderTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'HeaderTitle',
+  props: {
+    msg: String,
+  },
+
+  data() {
+    //
+  },
+
+  methods: {
+    //  
+  },
+
+  mounted() {
+    // 
+  }
+}
+</script>
+
+<template>
+  <h1>Vue Api: {{ msg }}</h1>
+</template>
