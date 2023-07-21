7ea53131d5de6578230304fb31a469c2417b7824
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-21 14:15:06 +0200

    fix

---
+++ src/views/AppProjectDetails.vue
@@ -0,0 +1,30 @@
+<script>
+import HeaderTitle from '../components/header/HeaderTitle.vue';
+import ProjectShow from '../components/project/ProjectShow.vue';
+import FooterNav from '../components/footer/FooterNav.vue';
+
+export default {
+    name: 'ProjectDetails',
+    components: {
+        HeaderTitle,
+        ProjectShow,
+        FooterNav,
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <header>
+            <HeaderTitle />
+        </header>
+
+        <main>
+            <ProjectShow />
+        </main>
+        
+        <footer>
+            <FooterNav />
+        </footer>
+    </div>
+</template>
