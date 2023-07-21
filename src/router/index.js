3eb2e383ba6828d88c9a258eee4daac2b6525418
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-21 12:04:47 +0200

    feat: route for project/Id and erorr page

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AppHome from '../views/AppHome.vue';
-import AppContact from '../views/AppContact.vue';
+import Home from '../views/AppHome.vue';
+import Contact from '../views/AppContact.vue';
 import ProjectList from '../views/AppProjectList.vue';
+import SingleProject from '../views/AppSingleProject.vue';
+import ErrorPage from '../views/errors/ErrorPage.vue';
+
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [  
         {
             path: '/',
             name: 'Home',
-            component: AppHome,
+            component: Home,
         },
         {
             path: '/contact',
             name: 'Contact',
-            component: AppContact,
+            component: Contact,
         },
         {
             path: '/projects',
             name: 'Projects',
             component: ProjectList,
         },
+        {
+            path: '/projects/:id',
+            name: 'SingleProject',
+            component: SingleProject,
+        },
+        {
+            path: '/error/:code',
+            name: 'Error',
+            component: ErrorPage,
+        },
     ],  
 });
 
 export default router;
-
