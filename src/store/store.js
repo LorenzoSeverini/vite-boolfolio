8428280dc5cd59907a3d261e9a8821fb36638ef9
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-21 12:05:01 +0200

    feat: store js file

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiUrl : 'http://localhost:8000/api/',
+    storageUrl : 'http://localhost:8000/storage/',
+    totalProjectsNumber : 0,
+});
